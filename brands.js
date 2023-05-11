1f09077451db8bbc446deda181a604df5b6b6564
Andres Cuellar <andrescuellar@Andress-Mini.home>
2023-05-10 23:33:44 -0400

    more styling

--- brands.js
+++ brands.js
@@ -4,10 +4,12 @@ let i = 0;
 let images = [];
 let time = 2500;
 
-images[0] = "images/Mazda-Logo.png";
+images[0] = "images/mazda-logo-0.png";
 images[1] = "images/Nissan_logo.png";
 images[2] = "images/mitsubishi-1-logo-png-transparent.png";
 images[3] = "images/toyota-logos-brands-logotypes-0.png";
+images[4] = "images/subaru-logo-2.png";
+images[5] = "images/honda-logo-png-19.png";
 
 const changeImg = () => {
   document.imgSlide.src = images[i];
